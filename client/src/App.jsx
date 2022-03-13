c01d6831048394e24946d9e9c99b55b3e88cc7db
Lengon <anhlangon1@gmail.com>
2022-03-13 07:45:53 +0700

    create login and register page

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './components/login.jsx';
+import Register from './components/register.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,60 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mysql = require('mysql');
 
-app.get('/trangchu', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 3001;
+
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: '',
+  database: 'login-register'
+});
+
+app.use(morgan('combined'))
+
+app.post('/register', (req, res) => {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.query(
+    "INSERT INTO member (username, password) VALUES (?,?)",
+    [username, password],
+    (err, result) => {
+      console.log(err);
+      result.send("Connect to");
+    })
+});
+
+
+app.post('/login', (req, res) => {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.query(
+    "SELECT id FROM member WHERE username = ? AND password = ?",
+    [username, password],
+    (err, result) => {
+      if(err){
+        res.send({err: err});
+      }
+      else{
+        if(result.length > 0){
+          res.send(result);
+        }
+        else{
+          res.send({message: "Sai tài khoản hoặc password !!!"});
+        }
+      }
+    }
+  )
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
